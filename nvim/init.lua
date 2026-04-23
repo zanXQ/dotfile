d8527c4ee767077db6591c302859c20ed146c442
zanXQ <popanda65@gmail.com>
2026-04-23 06:40:09 -0400

    add keymap for search top slang-server

--- nvim/init.lua
+++ nvim/init.lua
@@ -28,6 +28,9 @@ require('lazy').setup({
    require('plugins.indent-blankline'),
    require('plugins.misc'),
    require('plugins.conform'),
+   require('plugins.copilot'),
+   require('plugins.oil'),
+   require('plugins.slang-server'),
 }, {
   ui = {
     -- If you have a Nerd Font, set icons to an empty table which will use the
@@ -49,3 +52,51 @@ require('lazy').setup({
     },
   },
 })
+vim.cmd.colorscheme "catppuccin-macchiato"
+
+local function slang_fuzzy_set_top()
+    local builtin = require('telescope.builtin')
+    local actions = require('telescope.actions')
+    local action_state = require('telescope.actions.state')
+
+    -- 1. Find the Git root
+    local git_root = vim.fn.systemlist("git rev-parse --show-toplevel")[1]
+    if vim.v.shell_error ~= 0 then
+        print("Error: Not in a Git repository")
+        return
+    end
+
+    builtin.find_files({
+        results_title = "Select Top Level for Slang",
+        cwd = git_root,
+        no_ignore = true,
+        attach_mappings = function(prompt_bufnr, map)
+            actions.select_default:replace(function()
+                actions.close(prompt_bufnr)
+                local selection = action_state.get_selected_entry()
+                
+                if selection then
+                    local full_path
+                    -- Check if path is already absolute (starts with /)
+                    if selection.path:sub(1, 1) == "/" then
+                        full_path = selection.path
+                    else
+                        full_path = git_root .. "/" .. selection.path
+                    end
+                    
+                    -- Clean up any double slashes (//) just in case
+                    full_path = full_path:gsub("//+", "/")
+                    
+                    -- 3. Execute the Slang command
+                    vim.cmd("SlangServer setTopLevel " .. full_path)
+                    
+                    -- 4. Print the result
+                    print(" Slang Top-Level set to: " .. full_path)
+                end
+            end)
+            return true
+        end,
+    })
+end
+
+vim.keymap.set('n', '<leader>st', slang_fuzzy_set_top, { desc = "Fuzzy set Slang Top-Level" })
